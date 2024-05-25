40abe773994c4408c0b5c2bcc26908b142126071
guseynovsamed <samadaghash@code.edu.az>
2024-05-25 08:43:25 +0400

    BasketASP

---
+++ OneToMany/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OneToMany.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
+
---
+++ OneToMany/wwwroot/js/basket.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+
+    $(document).on("click", ".add-product-basket", function () {
+
+        let id = parseInt($(this).attr("data-id"));
+
+        $.ajax({
+            url: `home/addproducttobasket?id=${id}`,
+            type: 'post',
+            success: function (response) {
+
+                $(".rounded-circle").text(response.count);
+                $(".basket-total-price").text(`CART($${response.total})`);
+
+            }
+        })
+
+    })
+
+})
